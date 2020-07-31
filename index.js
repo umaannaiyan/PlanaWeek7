abc0b4aed564ada59643156902e783dfc49d0c6e
umaannaiyan <uma.annaiyan@gmail.com>
2020-07-31 18:44:44 +1000

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let addTaskNameInput=document.getElementById("#addTaskName").value;
+// var inputName = document.querySelector('input[type=text]');
+
+
+
+// let taskName= addTaskNameInput.oninput;
+// taskName.innerHTML=""
+
+console.log(addTaskNameInput);
+
